4500a73fd7fecff4ae874571991228e1281f472a
Hamza <hamzahda@outlook.com>
2020-04-05 12:49:54 +0200

    init

---
+++ services/Users/UserServiceTest.java
@@ -0,0 +1,8 @@
+package com.example;
+
+
+@SpringBootTest
+public class UserServiceTest {
+
+
+}
---
+++ services/Users/src/main/java/com/example/entity/User.java
@@ -0,0 +1,58 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="user")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    @Column
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    private String address;
+    
+
+	public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+
+}
