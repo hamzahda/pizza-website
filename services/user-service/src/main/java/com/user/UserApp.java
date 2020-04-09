12e5b3bb82caa272802911a1aec2642295f4069a
Hamza <hamzahda@outlook.com>
2020-04-09 14:46:29 +0200

    added some logging

--- services/user-service/src/main/java/com/user/UserApp.java
+++ services/user-service/src/main/java/com/user/UserApp.java
@@ -3,13 +3,19 @@ package com.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class UserApp {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(UserApp.class, args);
 
+	private static final Logger logger = LoggerFactory.getLogger(UserApp.class);
+	public static void main(String[] args) throws Exception {
+		logger.info("this is a info message");
+		SpringApplication.run(UserApp.class, args);
 		
 	}
+
 
 }
--- services/user-service/src/main/java/com/user/controller/UserController.java
+++ services/user-service/src/main/java/com/user/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.user.entity.User;
 import com.user.service.UserService;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,24 +23,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserController {
 
-@Autowired
-private UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    
+    @Autowired
+    private UserService userService;
 
 @GetMapping(value = "/")
 public ResponseEntity<List<User>> getAll() {
+    logger.info(" get request : get all users ");
     return ResponseEntity.ok(userService.getUser());   
 }
 
 @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 public ResponseEntity<User> getUser(@PathVariable Long id) {
     if (!userService.checkUser(id)) {
+        logger.error("the given user id: %d is not found", id);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
+    logger.info("get request : get user with the id : %d", id);
     return ResponseEntity.ok(userService.findById(id));
 }
 
 @PostMapping("/")
 public ResponseEntity<User> postUser (@RequestBody final User user){
+    logger.error("post request : create user");
     return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
 }
 
@@ -48,14 +55,17 @@ public ResponseEntity<String> deleteUser (@PathVariable final long id){
     if (!userService.checkUser(id)) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
+    logger.error("delete request:  %d user is deleted", id);
     return ResponseEntity.ok(String.format("User with ID : %d is deleted successfuly", id));
 }
 
 @PutMapping("/{id}")
 public ResponseEntity<User> modifyUser(@RequestBody final User user, @PathVariable final Long id) {
     if (!userService.checkUser(id)) {
+        logger.error("the given user id: %d is not found", id);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
+    logger.error("put request: %d user is modified ", id);
     return ResponseEntity.ok(userService.update(user));
   }
 
